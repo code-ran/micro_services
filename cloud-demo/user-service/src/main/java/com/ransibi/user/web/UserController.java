21343594f0ad81cec5e88fbd8fbc0ac4edc3632e
ransibi <rsb18252508590@163.com>
2025-03-04 17:48:09 +0800

    nacos学习

--- cloud-demo/user-service/src/main/java/com/ransibi/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/com/ransibi/user/web/UserController.java
@@ -4,7 +4,14 @@ import com.ransibi.user.pojo.User;
 import com.ransibi.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 @RestController
@@ -14,8 +21,18 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Value("${pattern.dateformat}")
+    private String dateFormat;
+
+
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+
+    @GetMapping("/nowTime")
+    public String getNowTime() {
+        SimpleDateFormat dateFormat1 = new SimpleDateFormat(dateFormat);
+        return dateFormat1.format(new Date());
     }
 }
