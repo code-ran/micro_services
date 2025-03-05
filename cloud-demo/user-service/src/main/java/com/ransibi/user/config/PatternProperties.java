f7798b9d873535259dcafa6d286e11486def09c6
ransibi <rsb18252508590@163.com>
2025-03-05 16:05:20 +0800

    nacos学习

---
+++ cloud-demo/user-service/src/main/java/com/ransibi/user/config/PatternProperties.java
@@ -0,0 +1,13 @@
+package com.ransibi.user.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    private String dateFormat;
+    private String share;
+}
--- cloud-demo/user-service/src/main/java/com/ransibi/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/com/ransibi/user/web/UserController.java
@@ -1,10 +1,12 @@
 package com.ransibi.user.web;
 
+import com.ransibi.user.config.PatternProperties;
 import com.ransibi.user.pojo.User;
 import com.ransibi.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,23 @@ import java.util.Date;
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @Value("${pattern.dateformat}")
-    private String dateFormat;
+//    @Value("${pattern.dateformat}")
+//    private String dateFormat;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+
+    @GetMapping("/share")
+    public PatternProperties getEnv(){
+        return patternProperties;
+    }
 
 
     @GetMapping("/{id}")
@@ -32,7 +44,7 @@ public class UserController {
 
     @GetMapping("/nowTime")
     public String getNowTime() {
-        SimpleDateFormat dateFormat1 = new SimpleDateFormat(dateFormat);
+        SimpleDateFormat dateFormat1 = new SimpleDateFormat(patternProperties.getDateFormat());
         return dateFormat1.format(new Date());
     }
 }
